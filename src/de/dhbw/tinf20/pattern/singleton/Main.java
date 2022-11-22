641f54a5a40f2640aab22d9c634523ada6afb46d
Daniel Lindner <daniel.lindner@softwareschneiderei.de>
2022-11-22 09:43:37 +0100

    Concurrency-Bug in Implementierung aufgedeckt

--- src/de/dhbw/tinf20/pattern/singleton/Main.java
+++ src/de/dhbw/tinf20/pattern/singleton/Main.java
@@ -1,21 +1,22 @@
 package de.dhbw.tinf20.pattern.singleton;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
 
 	public static void main(String[] args) {
-		//MySingleton neu = new MySingleton();
-		RandomnessSingleton verwendung = RandomnessSingleton.getInstance();
-		System.out.println(
-			verwendung.getZufallszahl()
-		);
-		
-		
+		ExecutorService threadpool = Executors.newFixedThreadPool(5);
+		for (int i = 0; i < 5; i++) {
+			threadpool.submit(Main::vorgang);
+		}
+		threadpool.shutdown();
 	}
 	
-//	public static void vorgang() {
-//		RandomnessSingleton verwendung = RandomnessSingleton.getInstance();
-//		System.out.println(
-//			verwendung.getZufallszahl()
-//		);
-//	}
+	public static void vorgang() {
+		RandomnessSingleton verwendung = RandomnessSingleton.getInstance();
+		System.out.println(
+			Thread.currentThread().getId() + ": " + verwendung.getZufallszahl()
+		);
+	}
 }
