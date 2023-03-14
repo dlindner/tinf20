6f685dd42e44fec6d0185d182da25ca24384a9e7
Daniel Lindner <daniel.lindner@softwareschneiderei.de>
2023-03-14 11:45:02 +0100

    Improved imports

--- src/de/dhbw/tinf20/mock/AutoTest.java
+++ src/de/dhbw/tinf20/mock/AutoTest.java
@@ -1,19 +1,20 @@
 package de.dhbw.tinf20.mock;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.Assert.*;
 
 import org.easymock.EasyMock;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 public class AutoTest {
 
 	@Test
 	public void geplante_Obsoleszenz() {
+		Motor mockedMotor = EasyMock.mock(Motor.class);
+		EasyMock.expect(mockedMotor.istDefekt()).andReturn(false);
+		EasyMock.replay(mockedMotor);
 		Auto target = new Auto(
 			11,
-			new Motor()
+			mockedMotor
 		);
 		assertThat(
 			target.istMotorwarnleuchteAn()
